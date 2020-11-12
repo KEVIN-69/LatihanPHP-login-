47719e367f207e7227d82ecb9315157769d8d456
KEVIN-69 <dnielkvin69@gmail.com>
2020-11-12 15:53:22 +0700

    first commit

--- Login.php
+++ Login.php
@@ -1,45 +1,71 @@
-<?php
-if (isset($_POST["login"])) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    $result = mysqli_query($conn,"SELECT * FROM user WHERE useraname = '$username'");
-
-    if(mysqli_num_rows($result) === 1 ){
-
-        $row = mysqli_fetch_assoc($result);
-        if (password_verify($password, $row["password"])){
-            header("Location: index.php")
-            exit;
-        }
-    }
-    $error = true ;
-}
-?>
 <!DOCTYPE html>
-<html>
 <head>
-    <title>Halaman Login</title>
-    <?php if (isset($error)):
-    ?>
-    <p style="color : red; font-style: italic;">username / password salah</p>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+   <title>Halaman Login</title>
 </head>
 <body>
-    <label> LOGIN </label>
-    <form action = ""method= "post">
-    <ul>
-        <li>
-            <label for = "username">Username :</label>
-            <input type= "text" name="username" id= "username">
-        </li>
-        <li>
-            <label for ="password">Password :</label>
-            <input type="password" name="password" id="password">
-        </li>
-        <li>
-            <button type="submit" name="login">Login</button>
-        </li>
-    </ul> 
-    </form>
+<?php
+session_start();
+require_once "koneksi.php";
+require_once "helper.php";
+if ($_SERVER["REQUEST_METHOD"]== "POST"){
+    //ambil data
+    $user = (isset($_POST['username'])) ? $_POST['username'] : "";
+    $pass = (isset($_POST['password'])) ? $_POST['password'] : "";
+    
+    //validasi
+    $errCount =0;
+    if ($user == "" || $pass ==""){
+        set_message("Kolom Inputan Username dan Password tidak boleh kosong");
+        $errCount +=1;
+    }
+
+    //cek login
+    if ($errCount == 0){
+        $pass = md5($pass);
+        $SQL = "SELECT * FROM user WHERE username='".$user."' AND password='".$pass."'";
+
+        $result = $conn ->query($SQL);
+        if ($result->num_rows > 0){
+            $u = $result->fetch_assoc();
+            
+            $_SESSION['login']['status'] = true;
+            $_SESSION['login']['username'] = $u['username'];
+            $_SESSION['login']['level'] = $u['level'];
+            
+            switch($u['level']){
+                case 'Admin' :
+                    header("location:admin.php");
+                    break;
+                    case 'Guru' :
+                    header("location:guru.php");
+                    break;
+                    case 'Siswa' :
+                    header("location:siswa.php");
+                    break;
+                default:
+                    #code---
+                    break;
+            }
+        } else{
+            set_message("Gagal Login! Coba Lagi .");
+        }
+
+    }
+
+}
+?>
+
+<h1>Halaman Login</h1>
+<hr>
+<?=show_message(); ?>
+<form action="" method="post">
+<label for="username">Username</label><br>
+<input type="text" name="username"><br>
+<label for="password">Password</label><br>
+<input type="text" name="password"><br>
+<input type="submit" value="login">
+</form>
+<a href ="index.php"><button>Kembali</button></a>
 </body>
 </html>
